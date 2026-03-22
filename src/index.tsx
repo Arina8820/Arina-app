d14d14ec0febb89ae81cf807457c34c2fcedb44b
Arina8820 <ourbhip@gmail.com>
2026-03-22 17:15:52 +0500

    deploy

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,10 @@ import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 
 // Create a new router instance
-const router = createRouter({ routeTree })
+const router = createRouter({ 
+  routeTree,
+  basepath: '/Arina-app/',
+})
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
