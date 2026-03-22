4a2e2f28cce60db008dd4ec4ea501f51fecf9a3b
Arina8820 <Arina8820@users.noreply.github.com>
2026-03-22 12:16:22 +0000

    deploy: d14d14ec0febb89ae81cf807457c34c2fcedb44b

--- assets/_id-CD4-VNv2.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-DuHqo4I_.js";function t(){return o.jsx("div",{children:'Hello "/notes/$id"!'})}export{t as component};
---
+++ assets/_id-DmAKbGGZ.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-De_aKHS_.js";function t(){return o.jsx("div",{children:'Hello "/notes/$id"!'})}export{t as component};
