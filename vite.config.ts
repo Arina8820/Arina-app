8c2b43bdd709309ed5acc2f49a156de0e3fb24a4
Arina8820 <ourbhip@gmail.com>
2026-03-22 15:33:28 +0500

    Deploy updated site

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,7 @@ export default defineConfig({
       autoCodeSplitting: true,
     }),
     react(), 
-    tailwindcss()],
+    tailwindcss()
+  ],
+  base: '/Arina-app/', // <- сюда впиши точное имя твоего репозитория на GitHub
 })
